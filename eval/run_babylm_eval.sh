a6961dd6fc3f208a41b5b6a8a260160f2e3c85b3
Jonas Mayer Martins <217778717+jonasmaymar@users.noreply.github.com>
2025-08-11 21:47:46 +0200

    New eval model

--- eval/run_babylm_eval.sh
+++ eval/run_babylm_eval.sh
@@ -4,8 +4,9 @@ trap 'echo "Error occurred in $0 at line $LINENO"; exit 1' ERR
 
 #MODEL_PATH="BabyLM-community/babylm-baseline-100m-gpt2"
 #MODEL_PATH="BabyLM-community/babylm-interaction-baseline-simpo"
-MODEL_PATH="llm-slice/babylm-gpt2-small-90M-seed42"
-REVISION_NAME="chck_1000M" # or 900 M
+#MODEL_PATH="llm-slice/babylm-gpt2-small-90M-seed42"
+MODEL_PATH="llm-slice/babylm-gpt2-small-90M-seed41_chck_200M_ppo-1600K-seed41"
+REVISION_NAME="main" # main, chck_1000M, or chck_900M
 BACKEND="causal"
 EVAL_SCRIPT="eval_zero_shot.sh"
 LOGDIR="logs/eval"
